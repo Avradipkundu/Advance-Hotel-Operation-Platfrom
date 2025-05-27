bac415a803fdc18f549eb87bfa78ab4cfeb90a93
Avradipkundu <avradipkundu851@gmail.com>
2025-05-27 17:55:47 +0530

    database connection

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const mongoURL = process.env.MONGODB_URI
+mongoose.connect(mongoURL,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+// define event listeners for database connection
+
+db.on('connected', () => {
+    console.log("Connected to MongoDB server")
+})
+db.on('error', (err) => {
+    console.log("MongoDB connection error: ",err)
+})
+db.on('disconnected', () => {
+    console.log("MongoDB disconnected")
+})
+
+// export the database connection
+module.exports = db
