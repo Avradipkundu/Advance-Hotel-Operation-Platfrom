ed37e2d864883dbc8b088fc3cf4ff7d53b254c51
Avradipkundu <avradipkundu851@gmail.com>
2025-06-11 10:29:29 +0530

    Multer

--- backend/middleware/Multer.js
+++ backend/middleware/Multer.js
@@ -1,11 +1,17 @@
 const multer = require('multer')
+const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/")
+        const uploadPath = "uploads/";
+        if (!fs.existsSync(uploadPath)) {
+            fs, fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath)
     },
     filename: (req, file, cb) => {
-        cb(null, suffix + "-" + file.originalname)
+        cb(null, Date.now() + "-" + path.extname(file.originalname))
     },
 })
 
